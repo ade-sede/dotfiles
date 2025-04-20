0031d065f649dc983c583045c1ed60bd56d95a44
Adrien DE SEDE <adrien.de.sede@gmail.com>
2025-04-20 21:11:12 +0000

    fix: don't load config when loading tmux pane switcher

--- scripts/tmux-switch-pane.sh
+++ scripts/tmux-switch-pane.sh
@@ -2,16 +2,18 @@
 
 export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#d0d0d0,bg:-1,hl:#5f87af --color=fg+:#d0d0d0,bg+:#262626,hl+:#5fd7ff --color=info:#afaf87,prompt:#d7005f,pointer:#af5fff --color=marker:#87ff00,spinner:#af5fff,header:#87afaf'
 
-# customizable
+MINIMAL_TMUX_CONFIG=$(mktemp)
+echo "" > $MINIMAL_TMUX_CONFIG
+
 LIST_DATA="#{session_name}"
 FZF_COMMAND="fzf-tmux -p --delimiter=: --with-nth 4 --color=hl:2"
 
-# do not change
 TARGET_SPEC="#{session_name}:#{window_id}:#{pane_id}:"
 
-# select pane
-LINE=$(tmux list-sessions -F "$TARGET_SPEC $LIST_DATA" | $FZF_COMMAND) || exit 0
-# split the result
+LINE=$(tmux -f $MINIMAL_TMUX_CONFIG list-sessions -F "$TARGET_SPEC $LIST_DATA" | $FZF_COMMAND) || exit 0
 args=(${LINE//:/ })
-# activate session/window/pane
-tmux select-pane -t ${args[2]} && tmux select-window -t ${args[1]} && tmux switch-client -t ${args[0]}
+tmux -f $MINIMAL_TMUX_CONFIG select-pane -t ${args[2]} && 
+tmux -f $MINIMAL_TMUX_CONFIG select-window -t ${args[1]} && 
+tmux -f $MINIMAL_TMUX_CONFIG switch-client -t ${args[0]}
+
+rm $MINIMAL_TMUX_CONFIG
