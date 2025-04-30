02818c185027b8f698b2effc11fc35604f79fee2
Adrien DE SEDE <adrien.de.sede@gmail.com>
2025-05-01 00:49:02 +0200

    greetings

--- dotfiles/fish/scripts/greeting.fish
+++ dotfiles/fish/scripts/greeting.fish
@@ -1,50 +1,58 @@
 function fish_greeting
-    set quotes (fortune)
-    if test $status -ne 0
-        set quotes "Welcome back!"
-    end
-    
-    set animal_options "default" "tux" "stegosaurus" "dragon" "sheep" "elephant" "moose" "koala"
-    set random_animal $animal_options[(random 1 (count $animal_options))]
-    
-    set greeting_option (random 1 10)
-    
+    set greeting_option (random 1 8)
+
     switch $greeting_option
         case 1
-            if test (random 1 3) -eq 1
-                cbonsai | lolcat
-            else
-                cbonsai
-            end
+	    if test (random 0 1) -eq 1
+	        cbonsai | lolcat
+	    else
+	        cbonsai
+	    end
         case 2
-            echo $quotes | cowsay -f $random_animal | lolcat
+            set quotes (fortune)
+            
+            set animal_options "default" "tux" "stegosaurus" "dragon" "sheep" "elephant" "moose" "koala"
+            set random_animal $animal_options[(random 1 (count $animal_options))]
+            
+            set box_designs cat dog bear mouse
+            set random_box $box_designs[(random 1 (count $box_designs))]
+            
+            set text_formatters figlet "boxes -d $random_box" "cowsay -f $random_animal"
+            set selected_formatter $text_formatters[(random 1 (count $text_formatters))]
+            if test (random 0 1) -eq 1
+                echo $quotes | eval $selected_formatter | lolcat
+            else
+                echo $quotes | eval $selected_formatter
+            end
         case 3
-            echo $quotes | figlet | lolcat
+            doge
         case 4
-            cmatrix -s -C blue -u 9 -B
-        case 5
-            sl
-        case 6
-            if type -q asciiquarium
-                asciiquarium -c -t 3
-            else
-                echo $quotes | cowsay -f $random_animal | lolcat
-            end
-        case 7
-            if type -q nyancat
-                nyancat -t 3
-            else
-                echo $quotes | cowsay -f $random_animal | lolcat
-            end
-        case 8
-            echo $quotes | boxes -d dog | lolcat
-        case 9
-            if type -q doge
-                doge
-            else
-                echo $quotes | cowsay -f $random_animal | lolcat
-            end
-        case 10
             neofetch
+        case 5
+	    cmatrix -s -C blue -u 9 -B
+        case 6
+	    set sl_options d g w l a f
+	    set random_options ""
+	    set random_options "$random_options -e"
+	    for opt in $sl_options
+	        if test (random 0 1) -eq 1
+	            set random_options "$random_options -$opt"
+	        end
+	    end
+	    if test "$random_options" = "-e"
+	        set random_opt $sl_options[(random 1 (count $sl_options))]
+	        set random_options "$random_options -$random_opt"
+	    end
+	    eval "sl $random_options"
+        case 7
+	    asciiquarium
+        case 8
+	    nyancat
+	case '*'
+	    if test (random 0 1) -eq 1
+	        echo "Oopsy, no greeting found" | figlet | lolcat
+	    else
+	        echo "Oopsy, no greeting found" | figlet
+	    end
     end
 end
