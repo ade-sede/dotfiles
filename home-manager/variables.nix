e74a98ab6d693fafd828248983537adf12a32f02
Adrien DE SEDE <adrien.de.sede@gmail.com>
2025-05-03 18:57:49 +0200

    wip

--- home-manager/variables.nix
+++ home-manager/variables.nix
@@ -1,6 +1,6 @@
 rec {
   userEmail = "adrien.de.sede@gmail.com";
   userName = "Adrien DE SEDE";
-  wallpaperDir = "/home/ade-sede/.dotfiles/wallpapers";
-  kdeDir = "/home/ade-sede/.dotfiles/KDE";
+  wallpaperDir = "~/.dotfiles/wallpapers";
+  kdeDir = "~/.dotfiles/KDE";
 }
