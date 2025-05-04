2ee820bff82b45b502381b57645da9405482f9f5
Adrien DE SEDE <adrien.de.sede@gmail.com>
2025-05-04 17:34:14 +0200

    experiment 2 with plasma manage

--- flake.nix
+++ flake.nix
@@ -7,12 +7,18 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     home-manager,
+    plasma-manager,
     ...
   } @ inputs: let
     lib = nixpkgs.lib;
@@ -33,6 +39,14 @@
             ./nixos/configuration.nix
             home-manager.nixosModules.home-manager
             {nixpkgs.config.allowUnfree = true;}
+            {
+              home-manager.sharedModules = [
+                plasma-manager.homeManagerModules.plasma-manager
+              ];
+              home-manager.users.${username}.imports = [
+                ./home-manager/plasma-config.nix
+              ];
+            }
           ];
         };
 
@@ -88,7 +102,9 @@
           };
 
           modules = [
+            inputs.plasma-manager.homeManagerModules.plasma-manager
             ./home-manager/home.nix
+            ./home-manager/plasma-config.nix
           ];
         };
 
