60613fad346ce59dfe96bb15004cfa593c7293fe
Adrien DE SEDE <adrien.de.sede@gmail.com>
2025-05-09 00:34:12 +0200

    feat: add obs

--- home-manager/linux-desktop.nix
+++ home-manager/linux-desktop.nix
@@ -25,4 +25,14 @@
         }
     )
   ];
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  };
+
 }
