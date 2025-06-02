64d0b08a8189edb6012f19d2ffafc5843aa68d37
Adrien DE SEDE <adrien.de.sede@gmail.com>
2025-06-02 22:07:32 +0200

    fish completion

--- dotfiles/fish/custom_config.fish
+++ dotfiles/fish/custom_config.fish
@@ -119,3 +119,8 @@ end
 if type -q fzf
   fzf --fish | source
 end
+if type -q scw
+  complete --erase --command scw
+  complete --command scw --no-files
+  complete --command scw --arguments '(scw autocomplete complete fish -- (commandline) (commandline --cursor) (commandline --current-token) (commandline --current-process --tokenize --cut-at-cursor))'
+end
