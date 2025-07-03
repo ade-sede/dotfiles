6c170e62f5c742b8c015b1d23533571bb0f8de88
Adrien DE SEDE <adrien.de.sede@gmail.com>
2025-07-04 01:04:04 +0200

    feat: open ports for steam link

--- hosts/koala-devbox/nixos/networking.nix
+++ hosts/koala-devbox/nixos/networking.nix
@@ -6,4 +6,21 @@
 }: {
   networking.hostName = "koala-devbox";
   networking.networkmanager.enable = true;
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [
+      # Steam Remote Play
+      27036
+      27037
+    ];
+    allowedUDPPorts = [
+      # Steam Remote Play
+      27031
+      27036
+      # Steam VR Streaming
+      10400
+      10401
+    ];
+  };
 }
