f9e982fdac780508bca3bce5119c8e635a4eab9f
Adrien DE SEDE <adrien.de.sede@gmail.com>
2025-09-01 05:07:14 +0200

    use pinentry curses on remote devbox

--- hosts/remote-devbox/home-manager/default.nix
+++ hosts/remote-devbox/home-manager/default.nix
@@ -7,6 +7,6 @@
   imports = [
     ../../../home-manager/common/home.nix
     ../../../home-manager/linux/packages.nix
-    ../../../home-manager/linux/gpg.nix
+    ./gpg-remote.nix
   ];
 }
---
+++ hosts/remote-devbox/home-manager/gpg-remote.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.gpg = {
+    enable = true;
+    settings = {
+      no-greeting = true;
+    };
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+    enableScDaemon = true;
+    defaultCacheTtl = 3600;
+    defaultCacheTtlSsh = 86400;
+    pinentry = {
+      package = pkgs.pinentry-curses;
+    };
+    extraConfig = ''
+      allow-loopback-pinentry
+      allow-preset-passphrase
+    '';
+  };
+
+  home.sessionVariables = {
+    SSH_AUTH_SOCK = "$(gpgconf --list-dirs agent-ssh-socket)";
+  };
+}
