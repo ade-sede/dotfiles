f3fea9415a332d1688474b06f2f6b91237d3948f
Adrien DE SEDE <adrien.de.sede@gmail.com>
2026-05-15 23:36:36 +0200

    Disable scdaemon on remote-devbox
    - No smartcard reader on a headless SSH machine
    - enableScDaemon injects 'grab' into gpg-agent.conf
    - 'grab' hijacks keyboard/TTY of active terminal apps (nvim, opencode)

--- hosts/remote-devbox/home-manager/gpg-remote.nix
+++ hosts/remote-devbox/home-manager/gpg-remote.nix
@@ -13,7 +13,6 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    enableScDaemon = true;
     defaultCacheTtl = 86400;
     defaultCacheTtlSsh = 86400;
     maxCacheTtl = 86400;
